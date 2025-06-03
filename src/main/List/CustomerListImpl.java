1e8819dd345128bdbad9e024a189bbe37139fe21
solmin <pinjelly@naver.com>
2025-06-04 03:03:38 +0900

    고객 수정

--- src/main/List/CustomerListImpl.java
+++ src/main/List/CustomerListImpl.java
@@ -16,7 +16,9 @@ public class CustomerListImpl implements CustomerList {
 
 	public boolean delete(String customerID){
 		try (DAO dao = new DAO()){
-			dao.executeQuery("DELETE FROM customer WHERE customer_id = ?", customerID);
+			dao.executeQuery("DELETE FROM event WHERE user_id = ?",customerID);
+			dao.executeQuery("DELETE FROM customer WHERE user_id = ?", customerID);
+			dao.executeQuery("DELETE FROM user WHERE user_id = ?",customerID);
 			return true;
 		}catch(Exception e){
 			return false;
@@ -25,6 +27,8 @@ public class CustomerListImpl implements CustomerList {
 
 	public boolean insert(Customer customer){
 		try (DAO dao = new DAO()){
+			dao.executeQuery("INSERT INTO user(user_id,user_pw,user_type_id) VALUES(?,?,?)",
+					customer.getCustomerID(), customer.getPw(), 1);
 			dao.executeQuery(
 					"INSERT INTO customer(user_id,account_number,address,age,job,name,phone_num,rrn,sex) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
 					customer.getCustomerID(),
@@ -44,7 +48,7 @@ public class CustomerListImpl implements CustomerList {
 
 	public Customer search(String customerID){
 		try (DAO dao = new DAO()){
-			return dao.executeQuery("SELECT * FROM customer WHERE customer_id = ?", customerID).toCustomer().get(0);
+			return dao.executeQuery("SELECT * FROM customer WHERE user_id = ?", customerID).toCustomer().get(0);
 		}catch (RuntimeException e){
 			return null;
 		}
@@ -56,7 +60,7 @@ public class CustomerListImpl implements CustomerList {
 
 	public boolean update(Customer updatedCustomer){
 		try (DAO dao = new DAO()){
-			dao.executeQuery("UPDATE customer SET user_id = ?, account_number = ?, address = ?, age,job = ?, name = ?, phone_num = ?, rrn = ?, sex = ? WHERE customer_id = ?",
+			dao.executeQuery("UPDATE customer SET account_number = ?, address = ?, age = ?,job = ?, name = ?, phone_num = ?, rrn = ?, sex = ? WHERE user_id = ?",
 					updatedCustomer.getAccountNumber(),
 					updatedCustomer.getAddress(),
 					updatedCustomer.getAge(),
